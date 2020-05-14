1e38ada8e55260ecee5a08cbf0e9a13884422da3
Cyrus Ingraham <cyrus.ingraham@jamf.com>
2020-05-14 18:07:32 -0500

    Added package to use ALPN branch for SPM

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(
+            url: "https://github.com/cyrusingraham/CocoaAsyncSocket.git",
+            .branch("Add-ALPN-support")
+        ),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.2"),
     ],
     targets: [
